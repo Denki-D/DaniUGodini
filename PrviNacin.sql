ad8b4f9b24f828850995d0db1a3271e120176bfe
Denis Gaščić <45295899+Denki-D@users.noreply.github.com>
2023-12-15 15:35:33 +0100

    Update PrviNacin.sql
    Dodana funkcija za izračun datuma Uskrsa preko koje se računaju i varijable za datume Uskrsnog ponedjeljka i Tijelova. Tkđ, dodane i varijable za nepomične blagdane. Sve je, dakle, u potpunosti automatizirano za tekuću godinu: potrebno je samo prekopirati skripta u Querry Editor i izvršiti.

--- PrviNacin.sql
+++ PrviNacin.sql
@@ -1,7 +1,46 @@
-﻿SET LANGUAGE hrvatski;
 SET LANGUAGE hrvatski;
+GO
+
+CREATE OR ALTER FUNCTION dbo.VratiDatumUskrsa (@godina INT) -- from: https://medium.com/@diangermishuizen/calculate-easter-sunday-dynamically-using-sql-447235cb8906
+RETURNS DATE
+AS
+BEGIN
+    DECLARE @varA TINYINT, @varB TINYINT, @varC TINYINT, @varD TINYINT, @varE TINYINT, @varF TINYINT, @varG TINYINT
+		  , @varH TINYINT, @varI TINYINT, @varK TINYINT, @varL TINYINT, @varM TINYINT,  @datumUskrsa DATE;    
+	SELECT @varA = @godina % 19, @varB = FLOOR(1.0 * @godina / 100), @varC = @godina % 100;
+    SELECT @varD = FLOOR(1.0 * @varB / 4), @varE = @varB % 4, @varF = FLOOR((8.0 + @varB) / 25);
+    SELECT  @varG = FLOOR((1.0 + @varB - @varF) / 3);
+    SELECT @varH = (19 * @varA + @varB - @varD - @varG + 15) % 30, @varI = FLOOR(1.0 * @varC / 4), @varK = @godina % 4;
+    SELECT  @varL = (32.0 + 2 * @varE + 2 * @varI - @varH - @varK) % 7;
+    SELECT @varM = FLOOR((1.0 * @varA + 11 * @varH + 22 * @varL) / 451);
+    SELECT  @datumUskrsa = DATEADD(dd, (@varH + @varL - 7 * @varM + 114) % 31, DATEADD(mm, FLOOR((1.0 * @varH + @varL - 7 * @varM + 114) / 31) - 1, DATEADD(yy, @godina - 2000, {d '2000-01-01' })));
+    RETURN @datumUskrsa;
+END;
+GO
 DECLARE @start DATE=dateadd(year, datediff(year, 0, getdate()), 0);
 DECLARE @end DATE=dateadd(year, datediff(year, 0, getdate()), 364);
+
+--NEPOMIČNI PRAZNICI
+DECLARE
+	@NovaGodina date = DATEFROMPARTS(YEAR(GETDATE()), 1, 1),
+	@SvetaTriKralja date = DATEFROMPARTS(YEAR(GETDATE()), 1, 6),
+	@PraznikRada date = DATEFROMPARTS(YEAR(GETDATE()), 5, 1),
+	@DanDržavnosti date = DATEFROMPARTS(YEAR(GETDATE()), 5, 30),
+	@DanAntifaBorbe date = DATEFROMPARTS(YEAR(GETDATE()), 6, 22),
+	@DanPobjede date = DATEFROMPARTS(YEAR(GETDATE()), 8, 5),
+	@VelikaGospa date = DATEFROMPARTS(YEAR(GETDATE()), 8, 15),
+	@SviSveti date = DATEFROMPARTS(YEAR(GETDATE()), 11, 1),
+	@DanSjecanjaNaVukovar date = DATEFROMPARTS(YEAR(GETDATE()), 11, 18),
+	@Bozic date = DATEFROMPARTS(YEAR(GETDATE()), 12, 25),
+	@SvetiStjepan date = DATEFROMPARTS(YEAR(GETDATE()), 12, 26);
+
+--POMIČNI PRAZNICI
+DECLARE 
+	@Uskrs date;
+	SELECT @Uskrs= dbo.VratiDatumUskrsa(YEAR(GETDATE()));
+DECLARE
+	@UskrsniPonedjeljak date = dateadd(day, 1, @Uskrs),
+	@Tijelovo date = dateadd(day, 60, @Uskrs);
 
 DROP TABLE IF EXISTS #datumi;
 
@@ -16,9 +55,9 @@ SELECT
       , [Dan u tjednu]=DATENAME(weekday, Dat)
       , [Radni dan]=
             CASE
-                  WHEN Dat IN ('2023/01/01', '2023/01/06','2023/04/09','2023/04/10'                   --prilagoditi
-                  , '2023/05/01','2023/05/30', '2023/06/08', '2023/06/22', '2023/08/05','2023/08/15',
-                  '2023/11/01','2023/11/18','2023/12/25','2023/12/26') THEN 'praznik'
+                  WHEN Dat IN (@NovaGodina, @SvetaTriKralja, @Uskrs, @UskrsniPonedjeljak,
+						@PraznikRada, @DanDržavnosti,@Tijelovo, @DanAntifaBorbe, @DanPobjede, @VelikaGospa,
+						@SviSveti, @DanSjecanjaNaVukovar, @Bozic, @SvetiStjepan) THEN 'praznik'
                   WHEN DATENAME(weekday, Dat) IN ('subota', 'nedjelja') THEN 'vikend'
                   ELSE 'RADNI DAN'
             END
@@ -57,5 +96,3 @@ SELECT Datum=CONVERT(varchar, Dat, 104)+'.'
 		END
 FROM #datumi
 order by dat
-
-
